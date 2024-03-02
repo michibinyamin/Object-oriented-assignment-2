84e8c7c65a708685c3e93758d6d2449a4bc8d59e
michibinyamin <118850024+michibinyamin@users.noreply.github.com>
2024-03-02 19:43:16 +0200

    post_image fixes

--- ImagePost.py
+++ ImagePost.py
@@ -8,12 +8,28 @@ class ImagePost(Post):
         super().__init__(user)  # Should be implemented here?
         self.photo = photo    # Holds the text of the post
 
-    def display(self):
-        print("\n"+self.photo)
+    #def display(self):
+        #print("\n"+self.photo)
         #image = Image.open(self.photo)
         #plt.imshow(image)
         #plt.axis('off')  # Turn off axis
         #plt.show()
+    def display(self):
+        print("\nShows picture")
+        try:
+            # Display image
+            img_matplotlib = plt.imread(self.photo)
+            plt.imshow(img_matplotlib)
+            plt.axis('off')
+            plt.show()
+
+            # Displaying image using Pillow
+            img_pillow = Image.open(self.photo)
+            img_pillow.show()
+            return "Displayed an image."
+
+        except FileNotFoundError:
+            return "Failed to display image."
 
     def __str__(self):
         return f"{self.uploader.name} posted a picture\n"
